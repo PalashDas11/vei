bde3270ad8dfc33ff07fbbcc72075f46c0eb89bd
PalashDas11 <pnpalash20@gmail.com>
2022-01-03 02:05:14 +0600

    simple website

---
+++ index.js
@@ -0,0 +1,62 @@
+const numbers = [45,4,9,16,25];
+
+// function myFunction(value,index,array){
+//        console.log(value);
+//        console.log(index);
+//        console.log(array);
+//        console.log("------");
+// } 
+
+// // arry method 
+// numbers.forEach(myFunction);
+// function myFunction(value){
+//     return value*2;
+
+// }
+
+//  const newArry = numbers.map(myFunction);
+//  console.log(numbers);
+//  console.log(newArry);
+
+//  filter method 
+
+// function myFilter(value,index,array){
+//     return value > 18  ;
+
+// }
+
+// const newArry = numbers.filter(myFilter);
+// console.log(numbers);
+// console.log(newArry);
+
+// reduce method 
+//  function myReduce(total,value, index, array){
+//      return (total);
+//  }
+
+//  const newRed = numbers.reduce(myReduce,);
+//  console.log(newRed);
+// //  reduceRight() count from right side 
+
+
+
+// every method(); this output is all value are the greter than value.
+
+// function myEvery( value , index, array){
+//     return value > 4
+// }
+// const newEvery = numbers.every(myEvery);
+// console.log(newEvery);
+
+//  some() method 
+
+//  function mySome( value , index, array){
+//     return value > 4
+// }
+// const newSome = numbers.some(mySome);
+// console.log(newSome);
+
+
+let indexof = numbers.indexOf(16) + 1;
+
+ console.log(indexof);
